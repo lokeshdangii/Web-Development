03c33c3b9d44ba0fe00292685414e776b8ce94e0
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-11 14:19:26 +0530

    JS

---
+++ JavaScript/variable.js
@@ -0,0 +1,14 @@
+console.log("JavaScript Variables");
+
+let a = 10;
+let name = "Lokesh"
+
+
+console.log(a);
+console.log(name);
+
+
+const numBer = 55;
+console.log(num);
+
+// const numBer = 45;
