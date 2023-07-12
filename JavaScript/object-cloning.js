93641fc398d2d033936b6b506b24f8e9e3bbe58f
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-12 17:41:02 +0530

    Object cloning in JS

---
+++ JavaScript/object-cloning.js
@@ -0,0 +1,28 @@
+// ---------------------  1. Iteration -------------------------
+
+let src = {
+    a:10, b:20,c:30
+};
+
+// let dest = {};
+
+// for (let key in src){
+//     dest[key] = src[key];
+// }
+
+
+// console.log(dest);
+
+
+// --------------------- 2. Assign -------------------------
+
+// let dest = Object.assign({},src);
+// console.log(dest);
+
+
+// 3 Spread
+
+let dest = {...src};
+console.log(dest);
+
+
