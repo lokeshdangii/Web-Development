1a1dbb697ed302d4470fe11a7195e6215472c88b
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-12 14:23:46 +0530

    Constructor in JS

--- JavaScript/constructor.js
+++ JavaScript/constructor.js
@@ -1,8 +1,8 @@
 // Constructor Function
 
-function Rectangle(len,bre){
-    this.length = len;
-    this.breadth = bre;
+function Rectangle(length,breadth){
+    this.length = length;
+    this.breadth = breadth;
     this.draw = function(){
         console.log("drawing");
     }
@@ -12,6 +12,25 @@ function Rectangle(len,bre){
 // Object creation using constructor function
 
 let rectanleObject = new Rectangle(4,6);
-console.log(rectanleObject);
+// console.log(rectanleObject);
 
+rectanleObject.color = "Yellow";
+// console.log(rectanleObject);
+
+delete rectanleObject.color;
+// console.log(rectanleObject);
+
+console.log(rectanleObject.constructor); 
+console.log(Rectangle.constructor);
+
+
+let Rectangle1 = new Function('length','breadth',`this.length = length;
+this.breadth = breadth;
+this.draw = function(){
+    console.log("drawing");
+}`);
+
+
+let obj1 = new Rectangle1(5,7);
+console.log(obj1);
 
