762e73b30f46e034bfe0074d27e89eaafebb0684
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-13 15:42:20 +0530

    Join and Split in JS

--- JavaScript/array5.js
+++ JavaScript/array5.js
@@ -13,4 +13,16 @@ let arr = [10,20,30,40,50];
 // })
 
 
-arr.forEach(number => console.log(number));
+// arr.forEach(number => console.log(number));
+
+
+// Join and Split
+
+let numbers = [2,4,6,8,10,12];
+let joined = numbers.join(' ');
+console.log(joined);
+
+let str = "this is my first message in JS";
+let splted = str.split(' ');
+console.log(splted);
+
