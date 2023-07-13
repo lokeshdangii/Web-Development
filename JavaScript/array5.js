432540366ceda19548625252b52c6aa3fa9a5ef6
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-13 15:37:51 +0530

    Iteratig array

---
+++ JavaScript/array5.js
@@ -0,0 +1,16 @@
+let arr = [10,20,30,40,50];
+
+
+// 1. for of loop
+// for (let value of arr){
+//     console.log(value);
+// }
+
+
+// 2. for Each loop
+// arr.forEach(function(number){
+//     console.log(number)
+// })
+
+
+arr.forEach(number => console.log(number));
