c8fe7b8585878115a5f9823bee829b9169201230
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-13 15:24:43 +0530

    Spread operatore in JS

---
+++ JavaScript/array4.js
@@ -0,0 +1,15 @@
+// Combining and Slicing an Array
+
+let first = [1,2,3,4];
+let second = [5,6,7,8];
+
+
+let combined = first.concat(second);
+console.log(combined);
+
+
+let sliced = combined.slice(2,4);
+console.log(sliced);
+
+
+console.log(combined);
---
+++ JavaScript/spreadOperator.js
@@ -0,0 +1,10 @@
+let first = [1,2,3];
+let second = [4,5,6];
+
+// let combined = [...first, ...second];
+
+let combined = [...first,'a',true ,...second,'b', false];
+console.log(combined);
+
+
+// spread operator is anothe way to combine arrays
