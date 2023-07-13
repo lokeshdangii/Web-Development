362f1fd8679bb2a6e529b89a701c933afe702722
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-13 14:41:35 +0530

    Removal of elements from array in JS

---
+++ JavaScript/array3.js
@@ -0,0 +1,20 @@
+// Removing Elements
+
+let numbers = [2,4,6,8,9,1,2,3];
+
+//End --> pop()
+numbers.pop();
+console.log(numbers);
+
+
+
+//Start --> shift()
+numbers.shift();
+console.log(numbers);
+
+
+// Mid --> splice()
+numbers.splice(2,2);    //2 is index and 2 is element delete count
+console.log(numbers);
+
+
