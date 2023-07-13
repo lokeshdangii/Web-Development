1e5ae828615b302697e8ec3f992a6754f59e0980
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-13 12:53:08 +0530

    Template Literal in JS

---
+++ JavaScript/templateLiteral.js
@@ -0,0 +1,13 @@
+// by using backtik we can print a string on console as it is we write in a program
+//  template literal = `  `;
+
+let str = 
+`Hii..
+
+This is JavaScript 
+
+Used for web Development as well as for the programming purpose
+`;
+
+
+console.log(str);
