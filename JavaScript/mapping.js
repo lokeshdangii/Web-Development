935a8e6f38a4f1828a5fad21ee8153fd9d632ee8
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-13 17:07:41 +0530

    Mapping & Chaining

--- JavaScript/mapping.js
+++ JavaScript/mapping.js
@@ -1,4 +1,25 @@
-let number = [1,2,3,4,5]
+// Mapping
+
+let number = [1,2,3,4,5];
 
 let mapped = number.map(value => "Student_no" + value);
-console.log(mapped);
+// console.log(mapped);
+
+
+// let number1 = [1,3,6,-2.-3];
+// let filtered = number1.filter(value => value >= 0);
+
+// let items = filtered.map(num => { value :num } );
+
+// console.log(items);
+
+
+
+
+// chaining
+
+let number1 = [1,3,6,-2, 4,-3];
+
+let items = number1.filter(value => value >= 0).map(num => { value : num } );
+
+console.log(items);
