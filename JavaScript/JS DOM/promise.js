54925d90e147676c563ea4c15e99ab2b6f75f765
lokeshdangii <lokeshdangi1045@gmail.com>
2023-07-19 14:54:11 +0530

    Promise

--- JavaScript/JS DOM/promise.js
+++ JavaScript/JS DOM/promise.js
@@ -2,14 +2,22 @@ let meraPromise1 = new Promise(function(resolve,reject){
     setTimeout(function(){
         console.log("I am in Promise1")
     },5000)
+
+    // resolve(23234);
+    reject(new Error('Error aa gayi'));
+    
 });
 
 
-let meraPromise2 = new Promise(function(resolve,reject){
-    setTimeout(function(){
-        console.log("I am in Promise1")
-    },5000)
-});
+meraPromise1.then((value)=> console.log(value));
+
+meraPromise1.catch((error) => {console.log("Recieved an error")});
+
+// let meraPromise2 = new Promise(function(resolve,reject){
+//     setTimeout(fun ction(){
+//         console.log("I am in Promise1")
+//     },5000)
+// });
 
 
-console.log("Pehlaa");
+console.log("Pehlaa");  
